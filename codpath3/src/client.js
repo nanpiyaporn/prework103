b19ec6c5631d8afae28671531d93c59961e37316
Piyaporn Puangprasert <dngooddeals.gmail.com>
2024-08-30 05:06:00 -0400

    added pages

--- codpath3/src/client.js
+++ codpath3/src/client.js
@@ -1,9 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
 
-const URL = 'insert your Project URL here';
+dotenv.config();
 
-const API_KEY = 'insert your Project API key here';
+const URL = 'https://doeepfuqfpqervgcpqsn.supabase.co';
+const API_KEY = process.env.APIKEY;
 
 const supabase = createClient(URL, API_KEY);
 
 export const supabase = createClient(URL, API_KEY);
+
